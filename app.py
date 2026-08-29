3271e336d8c1d048784200c8453470cdd21349cd
ritikdubey9586-cpu <ritikdubey9586@gmail.com>
2026-08-29 23:45:30 +0530

    just_added_file

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.title("🔄 Swapcase Tool")
 st.write("Convert uppercase letters to lowercase and lowercase letters to uppercase.")
 
-text = st.text_input("Enter your string")
+text = st.text_input("enter string  whatever you want ")
 st.write("Result:", text.swapcase())
